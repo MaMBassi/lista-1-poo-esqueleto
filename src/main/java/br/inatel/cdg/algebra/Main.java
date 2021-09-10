d36796fdc7ef89be8d0d19868119a9de4d86d2ae
MaMBassi <mariana.bassi@ges.inatel.br>
2021-09-10 17:18:02 -0300

    Resolvendo a Lista 01

--- src/main/java/br/inatel/cdg/algebra/Main.java
+++ src/main/java/br/inatel/cdg/algebra/Main.java
@@ -8,18 +8,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-//Nao precisa se preocupar com essa classe
+
 public class Main extends Application {
 
-    //Não precisa alterar nada dessa classe
     @Override
     public void start(Stage stage) throws Exception {
         new ScenePrincipal().criaScenePrincipal(stage);
     }
 
-    //Não precisa alterar nada dessa classe
     public static void main(String[] args) {
         launch();
     }
 }
-//Naoooooooooo
